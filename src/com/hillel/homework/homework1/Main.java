9002ca0420060b4d50075a2f6304f8a81dc98eaa
Dezimund <de3mond15@gmail.com>
2021-12-01 19:54:02 +0200

    Homework 1

---
+++ src/com/hillel/homework/homework1/Main.java
@@ -0,0 +1,16 @@
+package com.hillel.homework.homework1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("\nTown House Galleria, Milan Italy");
+        double x = 45.46586702656775;
+        System.out.println(x);
+
+        double y = 9.190071915847163;
+        System.out.println(y);
+
+
+    }
+}
